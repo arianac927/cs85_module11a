2c5437d5bd2f55b5debdeb5bbcbcd8f037d92c45
Ariana Castillo-Jara <ariana13castillo@gmail.com>
2026-07-31 13:23:31 -0700

    WeatherController updated

--- app/Http/Controllers/WeatherController.php
+++ app/Http/Controllers/WeatherController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
+
 use Illuminate\Http\Request;
 
 class WeatherController extends Controller
 {
-    //
+    public function index()
+    {
+        $json = Storage::get('weather.json');
+        $weatherData = json_decode($json, true);
+
+        return view('weather.index', ['weather' => $weatherData]);
+    }
 }
